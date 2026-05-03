f955fbc976f6623f390032264b03519642f1402a
GORE <149291163+GORE-MODS@users.noreply.github.com>
2026-05-03 15:56:16 -0500

    Add files via upload

---
+++ FUCKGRAYZONEMANAGER.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using GorillaLocomotion;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace FuckGrayZoneManager
+{
+    [HarmonyPatch(typeof(GreyZoneManager))]
+    public class FUCKGRAYZONEMANAGER
+    {
+        [HarmonyPatch("ActivateGreyZoneLocal")]
+        [HarmonyPrefix]
+        static bool BlockActivation() => false;
+
+        [HarmonyPatch("LocalSimpleActivation")]
+        [HarmonyPrefix]
+        static bool BlockSimple(bool onOff) => false;
+
+        [HarmonyPatch("get_GreyZoneActive")]
+        [HarmonyPrefix]
+        static bool ForceInactive(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+
+        [HarmonyPatch("get_GreyZoneAvailable")]
+        [HarmonyPrefix]
+        static bool ForceUnavailable(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+
+        [HarmonyPatch("OnPhotonSerializeView")]
+        [HarmonyPrefix]
+        static void SilentOverride(GreyZoneManager __instance)
+        {
+            FieldInfo activeField = typeof(GreyZoneManager).GetField("greyZoneActive", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (activeField != null)
+            {
+                activeField.SetValue(__instance, false);
+            }
+        }
+
+        [HarmonyPatch("GravityOverrideFunction")]
+        [HarmonyPrefix]
+        static bool NoGravity(GTPlayer player) => false;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using GorillaLocomotion;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace FuckGrayZoneManager
+{
+    [BepInPlugin("com.gore.fuckgreyzonemanager", "FuckGreyZoneManager", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            var harmony = new Harmony("com.gore.fuckgreyzonemanager");
+            harmony.PatchAll();
+        }
+    }
+}
